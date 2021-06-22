686b815f30afb56db8c9f35eb1c11072d9ed686d
Matthias Endres <git@me23.de>
2021-06-22 19:12:40 +0200

    remove dist folder

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { ServiceBroker } from "moleculer";
-export default function tracingBrokerCalls(name?: string): {
-    call(next: any): (this: ServiceBroker, actionName: string, params: any, opts: any) => any;
-};
