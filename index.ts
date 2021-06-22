9809778e1b42c1db867607b727587326a5a19955
Matthias Endres <git@me23.de>
2021-06-22 19:10:11 +0200

    Pass parente tracing span through calls, to trace all call over remote services

--- index.ts
+++ index.ts
@@ -4,11 +4,25 @@ export default function tracingBrokerCalls(name: string = "no-service") {
 	const spanOptions = { service: { fullName: name } };
 	return {
 		call(next: any) {
-			return function( this: ServiceBroker, actionName: string, params: any, opts: any ) {
+			return function( this: ServiceBroker, actionName: string, params: any, opts: any = {} ) {
 				let span: any;
+				let spanOpt: {[k: string]: any} = Object.assign(spanOptions, {}); //clone spanOptions for each call
+				
+				//set parent span-infos if available, so the request could be 
+				//traced through all services
+		        if (opts.requestID != null) {
+                    spanOpt.requestID = opts.requestID;
+                }
+                if(opts.parentCtx != null && opts.parentCtx.span != null && opts.parentCtx.span.id != null){
+                    spanOpt.parentID = opts.parentCtx.span.id;
+                    spanOpt.traceID = opts.parentCtx.span.traceID;
+                    spanOpt.tracing = opts.parentCtx.span.sampled;
+                    //spanOpt.requestID = spanOpt.requestID || opts.parentCtx.span.requestID;
+                } 
+
 				if ( this && this.hasOwnProperty("tracer") && false !== this.tracer.isEnabled()  ){
-					span = this.tracer.startSpan("calling: " + actionName, spanOptions );
-					opts = opts || {};
+					span = this.tracer.startSpan("calling: " + actionName, spanOpt );
+					
 					opts.parentSpan = span;
 				}
 				return next(actionName, params, opts)
--- tests/test-index.test.ts
+++ tests/test-index.test.ts
@@ -36,6 +36,7 @@ describe("A broker call", () => {
 				});
 		});
 	});
+
 	it("should add a span", async () => {
 		let span: any;
 		const sBroker = new ServiceBroker({
@@ -62,6 +63,46 @@ describe("A broker call", () => {
 						expect(res).toBe("pong");
 						expect(span.name).toBe("calling: service.ping");
 						expect(span.service.fullName).toBe("invoker");
+						resolve();
+					});
+				})
+				.catch((err: any) => {
+					reject(err);
+				});
+		});
+	});
+
+
+	it("should add a span with parentInfos", async () => {
+		let span: any;
+		const sBroker = new ServiceBroker({
+			logLevel: 'fatal',
+			middlewares: [middleware("invoker") as Middleware],
+			tracing: {
+				enabled: true,
+				stackTrace: true,
+			}
+		});
+		const service = sBroker.createService({
+			name: "service",
+			actions: {
+				ping(ctx: any) {
+					span = ctx.options.parentSpan;
+					return Promise.resolve("pong");
+				}
+			}
+		});
+		return new Promise<void>((resolve, reject) => {
+			(sBroker.start() as Promise<any>)
+				.then(() => {
+					const parentSpan = sBroker.tracer.startSpan("calling: ", {});
+					const opts = {parentCtx: {span: parentSpan}};
+					//@ts-ignore - opts missing values...
+					sBroker.call("service.ping", null, opts).then((res: any) => {
+						expect(res).toBe("pong");
+						expect(span.name).toBe("calling: service.ping");
+						expect(span.service.fullName).toBe("invoker");
+						expect(span.parentID).toEqual(parentSpan.id);
 						resolve();
 					});
 				})
